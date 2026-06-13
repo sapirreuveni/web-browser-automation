a0b69bf7d8222fa1a56f8dc695c975b6e7949212
sapirreuveni <sapir3173@gmail.com>
2026-06-13 19:54:17 +0300

    Fix clear_cart to use wait_for_timeout instead of networkidle

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -67,11 +67,11 @@ class CartPage(HeaderComp, BasePage):
 
     def clear_cart(self):
         self.page.goto("https://tutorialsninja.com/demo/index.php?route=checkout/cart")
-        self.page.wait_for_load_state("networkidle")
+        self.page.wait_for_load_state("domcontentloaded")
         while True:
             delete_buttons = self.page.locator(self.__DELETE_ITEM_BUTTON)
             if delete_buttons.count() == 0:
                 break
             delete_buttons.first.click()
-            self.page.wait_for_load_state("networkidle")
+            self.page.wait_for_timeout(2000)
         self.go_home()
